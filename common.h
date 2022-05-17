dfba0173ec996cbd65be4820f2f3ac91d3d5d7f9
William Beordo <104066027+williambeordo@users.noreply.github.com>
2022-05-18 01:37:08 +0200

    Delete common.h

--- common.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-namespace common {
-    const unsigned long long num_steps = 500000000; //number of x bins
-
-    void print_results(double pi, int n_threads){
-            printf("Pi value: %.20f\n Number of steps: %llu\n Number of threads: %d\n",pi,num_steps,n_threads);
-    }
-}
